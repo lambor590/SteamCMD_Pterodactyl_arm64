490871070cac96eae680799fada69fdfb6483ec1
lambor590 <lambor590@gmail.com>
2024-09-14 23:36:28 +0200

    second try

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,11 @@ export INTERNAL_IP=`hostname -I | awk '{print $1}'`
 
 # Update Source Server
 if [ ! -z ${SRCDS_APPID} ]; then
-    /bin/box64 ./steamcmd/steamcmd.sh +force_install_dir /home/container +login anonymous +app_update ${SRCDS_APPID} +quit
+    /usr/local/bin/box64 ./steamcmd/steamcmd.sh +force_install_dir /home/container +login anonymous +app_update ${SRCDS_APPID} +quit
 fi
 
 # Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo /bin/box64 ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+MODIFIED_STARTUP=`eval echo $(echo /usr/local/bin/box64 ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
