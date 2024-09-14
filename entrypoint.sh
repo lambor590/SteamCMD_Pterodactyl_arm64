d788ba95480d4e5c1cda31b805909a99d1544210
lambor590 <lambor590@gmail.com>
2024-09-15 00:39:07 +0200

    Cleanup entrypoint script: remove unused directory listing for /usr/local/bin during server update process

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,6 @@ export INTERNAL_IP=`hostname -I | awk '{print $1}'`
 
 # Update Source Server
 if [ ! -z ${SRCDS_APPID} ]; then
-    ls -la /usr/local/bin
     ./steamcmd/steamcmd.sh +force_install_dir /home/container +login anonymous +app_update ${SRCDS_APPID} +quit
 fi
 
