db949dbe513f11fb30854925ea9422bdcfd377b7
lambor590 <lambor590@gmail.com>
2024-09-15 00:53:34 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,15 +3,16 @@ cd /home/container
 sleep 1
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`hostname -I | awk '{print $1}'`
+export PATH="/home/container/box64:$PATH"
 
 # Update Source Server
 if [ ! -z ${SRCDS_APPID} ]; then
-    ./steamcmd/steamcmd.sh +force_install_dir /home/container +login anonymous +app_update ${SRCDS_APPID} +quit
+    /home/container/box64/box64 /home/container/steamcmd/steamcmd.sh +force_install_dir /home/container +login anonymous +app_update ${SRCDS_APPID} +quit
 fi
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
-# Run the Server
+# Run the Server    
 eval ${MODIFIED_STARTUP}
