43c1e2009f4a9670dab2a8bf89cd6854dcac8d00
lambor590 <lambor590@gmail.com>
2024-09-15 00:38:42 +0200

    Fix entrypoint script: remove box64 from steamcmd call and update modified startup variable handling

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,11 +7,11 @@ export INTERNAL_IP=`hostname -I | awk '{print $1}'`
 # Update Source Server
 if [ ! -z ${SRCDS_APPID} ]; then
     ls -la /usr/local/bin
-    /bin/box64 ./steamcmd/steamcmd.sh +force_install_dir /home/container +login anonymous +app_update ${SRCDS_APPID} +quit
+    ./steamcmd/steamcmd.sh +force_install_dir /home/container +login anonymous +app_update ${SRCDS_APPID} +quit
 fi
 
 # Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo /bin/box64.sh ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
