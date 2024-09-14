44a93c8098ed27b5825fa707a3c04b939edc94a2
lambor590 <lambor590@gmail.com>
2024-09-14 19:46:26 +0200

    Fix path to steamcmd in entrypoint.sh for proper server updates

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ export INTERNAL_IP=`hostname -I | awk '{print $1}'`
 
 # Update Source Server
 if [ ! -z ${SRCDS_APPID} ]; then
-    /usr/local/bin/box64.sh ./steamcmd/steamcmd.sh +force_install_dir /home/container +login anonymous +app_update ${SRCDS_APPID} +quit
+    ./steamcmd/steamcmd.sh +force_install_dir /home/container +login anonymous +app_update ${SRCDS_APPID} +quit
 fi
 
 # Replace Startup Variables
